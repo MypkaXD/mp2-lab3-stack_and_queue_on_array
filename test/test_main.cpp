7d23aa90f8a9d3596e2eda1fd458568a269a5832
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-11-13 17:12:38 +0300

    Add files via upload

---
+++ test/test_main.cpp
@@ -0,0 +1,14 @@
+#include <gtest.h>
+#include "Stack.h"
+#include "Queue.h"
+#include "Vector.h"
+#include <string>
+
+
+int main(int argc, char** argv)
+{
+
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+
+}
